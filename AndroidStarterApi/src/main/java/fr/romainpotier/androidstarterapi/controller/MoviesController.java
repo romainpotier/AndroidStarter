8127ad882dbef3216fa944f164981035497d10a4
romain_potier <rpotier.prestataire@voyage-sncf.com>
2014-04-25 16:00:20 +0200

    Change @Controller annotation

--- AndroidStarterApi/src/main/java/fr/romainpotier/androidstarterapi/controller/MoviesController.java
+++ AndroidStarterApi/src/main/java/fr/romainpotier/androidstarterapi/controller/MoviesController.java
@@ -5,14 +5,13 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.romainpotier.androidstarterapi.beans.Movie;
 import fr.romainpotier.androidstarterapi.service.MovieService;
 
-@Controller
+@RestController
 @RequestMapping(value = "/movies")
 public class MoviesController {
 
@@ -22,7 +21,6 @@ public class MoviesController {
     MovieService movieService;
 
     @RequestMapping(produces = "application/json")
-    @ResponseBody
     public List<Movie> getMovies() {
         return movieService.getMovies();
     }
