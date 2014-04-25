efec280db5f32f60f96c274c70d2acb27f268c51
romain_potier <rpotier.prestataire@voyage-sncf.com>
2014-04-25 15:44:07 +0200

    Call rest webservice

--- AndroidStarter/src/main/java/fr/romainpotier/androidstarter/AndroidStarter.java
+++ AndroidStarter/src/main/java/fr/romainpotier/androidstarter/AndroidStarter.java
@@ -1,32 +1,55 @@
 package fr.romainpotier.androidstarter;
 
+import java.util.List;
+
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.rest.RestService;
 
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import fr.romainpotier.androidstarter.beans.Movie;
+import fr.romainpotier.androidstarter.service.MoviesService;
 
+/**
+ * Simple activity for testing REST webservice
+ * 
+ * @author Romain Potier
+ */
 @EActivity(R.layout.activity_main)
 public class AndroidStarter extends Activity {
 
     @ViewById
     TextView textView;
 
-    // @RestService
-    // MoviesService moviesService;
+    @ViewById
+    ListView listView;
+
+    @RestService
+    MoviesService moviesService;
 
     @Click
     void callRestService() {
-        // List<Movie> movies = moviesService.getMovies();
-        // Log.d("MyTag", movies.get(0).getTitle());
+        callMoviesService();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        getMenuInflater();
-        return true;
+    @Background
+    void callMoviesService() {
+        // Call service
+        List<Movie> movies = moviesService.getMovies();
+        updateListView(movies);
+    }
+
+    @UiThread
+    void updateListView(final List<Movie> movies) {
+        // Create custom adapter
+        final ArrayAdapter<Movie> arrayAdapter = new ArrayAdapter<Movie>(this, android.R.layout.simple_list_item_1, movies);
+        listView.setAdapter(arrayAdapter);
     }
 
 }
--- AndroidStarter/src/main/java/fr/romainpotier/androidstarter/service/MoviesService.java
+++ AndroidStarter/src/main/java/fr/romainpotier/androidstarter/service/MoviesService.java
@@ -1,9 +1,21 @@
 package fr.romainpotier.androidstarter.service;
 
+import java.util.List;
 
-//@Rest(rootUrl = "http://176.31.110.216:99/androidstarter", converters = { MappingJacksonHttpMessageConverter.class })
+import org.androidannotations.annotations.rest.Get;
+import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+
+import fr.romainpotier.androidstarter.beans.Movie;
+
+/**
+ * REST webservice example
+ * 
+ * @author Romain Potier
+ */
+@Rest(rootUrl = "http://176.31.101.144:99", converters = { MappingJacksonHttpMessageConverter.class })
 public interface MoviesService {
 
-    // @Get("/movies")
-    // List<Movie> getMovies();
+    @Get("/movies")
+    List<Movie> getMovies();
 }
